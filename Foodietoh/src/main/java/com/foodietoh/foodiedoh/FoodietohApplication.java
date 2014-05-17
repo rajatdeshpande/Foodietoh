facb5c3962920073ef023729aac7497c9c5d500b
Rajat Deshpande <rajatdeshpande@gmail.com>
2014-05-17 14:09:42 +0530

    Initial commit

---
+++ Foodietoh/src/main/java/com/foodietoh/foodiedoh/FoodietohApplication.java
@@ -0,0 +1,11 @@
+package com.foodietoh.foodiedoh;
+
+import android.app.Application;
+
+/**
+ * Created by rajatdeshpande on 4/26/14.
+ */
+public class FoodietohApplication extends Application {
+
+
+}
---
+++ Foodietoh/src/main/java/com/foodietoh/foodiedoh/ListElement.java
@@ -0,0 +1,17 @@
+package com.foodietoh.foodiedoh;
+
+/**
+ * Created by rajatdeshpande on 4/26/14.
+ */
+public class ListElement {
+
+    public String dayOfWeek;
+    public int price;
+    public boolean isChecked;
+
+    public ListElement(String dayOfWeek, int price, boolean isChecked) {
+        this.dayOfWeek = dayOfWeek;
+        this.price = price;
+        this.isChecked = isChecked;
+    }
+}
